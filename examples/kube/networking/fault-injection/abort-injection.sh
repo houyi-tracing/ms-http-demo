99bffbcd77d5fde589c4fb95622dfe748556c95d
yan-fuhai <yan_fuhai@hotmail.com>
2020-12-12 18:16:24 +0800

    Update scripts

--- examples/kube/networking/fault-injection/abort-injection.sh
+++ examples/kube/networking/fault-injection/abort-injection.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 SERVICE_NAME=$1
-PERCENTAGE=$2
+URI=$2
+PERCENTAGE=$3
 
 if [[ -z $SERVICE_NAME ]]; then
   echo "Service name cannot be empty"
@@ -9,7 +10,7 @@ if [[ -z $SERVICE_NAME ]]; then
 fi
 
 file=ms-abort.yaml
-sed 's/SERVICE_NAME/'${SERVICE_NAME}'/g' $file | sed 's/ABORT_PERCENTAGE/'${PERCENTAGE}'/g' | kubectl apply -f -
+sed 's/SERVICE_NAME/'${SERVICE_NAME}'/g' $file | sed 's/ABORT_PERCENTAGE/'${PERCENTAGE}'/g' | sed 's/URI/'${URI}'/g' | kubectl apply -f -
 
 echo "SERVICE_NAME=${SERVICE_NAME}"
 echo "PERCENTAGE=${PERCENTAGE}"
