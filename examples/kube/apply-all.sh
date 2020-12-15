b5fd831852e3509e79497214734dd466c3c4bc9d
yan_fuhai@foxmail.com <yan_fuhai@foxmail.com>
2020-12-15 15:36:56 +0800

    Update script

--- examples/kube/apply-all.sh
+++ examples/kube/apply-all.sh
@@ -22,12 +22,7 @@ else
       echo "SAMPLING_RATE must be set while using probability sampler"
       exit 0
     else
-      if [[ 0 <= $sampling_rate && $sampling_rate <= 1 ]]]; then
-        echo "SAMPLING_RATE=${sampler_rate}"
-      else
-        echo "Invalid SAMPLING_RATE: ${sampling_rate} (must be in range [0, 1])"
-        exit 0
-      fi
+      echo "SAMPLING_RATE=${sampler_rate}"
     fi
     ;;
   "const")
@@ -35,12 +30,7 @@ else
       echo "ALWAYS_SAMPLE must be set while using const sampler"
       exit 0
     else
-      if [[ $always_sample -ne "true" && $always_sample -ne "false" ]]; then
-        echo "Invalid ALWAYS_SAMPLE: ${always_sample} (must be \"true\" of \"false\")"
-        exit 0
-      else
-        echo "ALWAYS_SAMPLE=${always_sample}"
-      fi
+      echo "ALWAYS_SAMPLE=${always_sample}"
     fi
     ;;
     "rate-limit")
@@ -48,18 +38,14 @@ else
       echo "MAX_TRACES_PER_SECOND must be set while using rate-limit sampler"
       exit 0
     else
-      if [[ $max_traces_per_second < 0 ]]; then
-        echo "Invalid MAX_TRACES_PER_SECOND: ${max_traces_per_second} (must >= 0)"
-        exit 0
-      else
-        echo "MAX_TRACES_PER_SECOND=${max_traces_per_second}"
-      fi
+      echo "MAX_TRACES_PER_SECOND=${max_traces_per_second}"
     fi
     ;;
     *)
       echo "Unsupported sampler type: ${sampler_type}"
       exit 0
     ;;
+    esac
 fi
 
 
