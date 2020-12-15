e161f5e2b361a49524dd4ccdcbe940e73ce35b78
yan-fuhai <yan_fuhai@hotmail.com>
2020-12-15 15:40:56 +0800

    Update scripts

--- examples/kube/apply-all.sh
+++ examples/kube/apply-all.sh
@@ -1,65 +1,57 @@
 #!/bin/bash
 
-log_level=$LOG_LEVEL
-sampler_type=$SAMPLER_TYPE
-sampling_rate=$SAMPLING_RATE
-always_sample=$ALWAYS_SAMPLE
-max_traces_per_second=$MAX_TRACES_PER_SECOND
-
-if [[ -z $log_level ]]; then
-  log_level=info
+if [[ -z $LOG_LEVEL ]]; then
+  LOG_LEVEL=info
 fi
 
-if [[ -z $sampler_type ]]; then
-  sampler_type=dynamic
+if [[ $SAMPLER_TYPE ]]; then
+  SAMPLER_TYPE=dynamic
   echo "Using default sampler type: dynamic"
 else
-  case $sampler_type in
+  case $SAMPLER_TYPE in
   "dynamic")
     ;;
   "prob")
-    if [[ -z $sampling_rate ]]; then
+    if [[ $SAMPLING_RATE ]]; then
       echo "SAMPLING_RATE must be set while using probability sampler"
       exit 0
     else
-      echo "SAMPLING_RATE=${sampler_rate}"
+      echo "SAMPLING_RATE=${SAMPLING_RATE}"
     fi
     ;;
   "const")
-    if [[ -z $always_sample ]]; then
+    ALWAYS_SAMPLE ]]; then
       echo "ALWAYS_SAMPLE must be set while using const sampler"
       exit 0
     else
-      echo "ALWAYS_SAMPLE=${always_sample}"
+      echo "ALWAYS_SAMPLE=${ALWAYS_SAMPLE}"
     fi
     ;;
     "rate-limit")
-    if [[ -z $max_traces_per_second ]]; then
+    if [[ $MAX_TRACES_PER_SECOND ]]; then
       echo "MAX_TRACES_PER_SECOND must be set while using rate-limit sampler"
       exit 0
     else
-      echo "MAX_TRACES_PER_SECOND=${max_traces_per_second}"
+      echo "MAX_TRACES_PER_SECOND=${MAX_TRACES_PER_SECOND}"
     fi
     ;;
     *)
-      echo "Unsupported sampler type: ${sampler_type}"
+      echo "Unsupported sampler type: ${SAMPLER_TYPE}"
       exit 0
     ;;
     esac
 fi
 
-
-echo "LOG_LEVEL=${log_level}"
-echo "SAMPLER_TYPE=${sampler_type}"
+echo "LOG_LEVEL=${LOG_LEVEL}"
+echo "SAMPLER_TYPE=${SAMPLER_TYPE}"
 
 cd ~/github/ms-http-demo/examples/kube/
-git pull
 files=$(ls ./ | grep "^ms-.*\.yaml$")
 for f in $files; do
-  sed 's/log_level/'${log_level}'/g' $f | \
-  sed 's/sampler_type/'${sampler_type}'/g' | \
-  sed 's/sampling_rate/'${sampler_rate}'/g' | \
-  sed 's/always_sample/'${always_sample}'/g' | \
-  sed 's/max_traces_per_second/'${max_traces_per_second}'/g' | \
+  sed 's/LOG_LEVEL/'${LOG_LEVEL}'/g' $f | \
+  sed 's/SAMPLER_TYPE/'${SAMPLER_TYPE}'/g' | \
+  sed 's/SAMPLING_RATE/'${SAMPLING_RATE}'/g' | \
+  ALWAYS_SAMPLE}'/g' | \
+  sed 's/MAX_TRACES_PER_SECOND/'${MAX_TRACES_PER_SECOND}'/g' | \
   kubectl apply -f -
 done
